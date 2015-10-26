9bb6e27ed42042014a0bfa46c8345d257d1e7e93
Gabriel Malet <gabrielmalet@gmail.com>
2015-10-26 10:07:37 +0100

    Fix less build for bootsrap

--- gulp-tasks/bootstrap.js
+++ gulp-tasks/bootstrap.js
@@ -2,10 +2,10 @@
 
 module.exports = function(gulp, plugins, npmPackages, config) {
   return function() {
-    gulp.src('src/client/less/variables.less')
+    gulp.src('src/client/less/bootstrap/variables.less')
       .pipe(gulp.dest(config.path.public + '/lib/bootstrap/less'));
 
-    gulp.src(config.path.public + '/lib/bootstrap/less/bootstrap.less')
+    return gulp.src(config.path.public + '/lib/bootstrap/less/bootstrap.less')
       .pipe(plugins.less())
       .pipe(gulp.dest(config.path.public + '/lib/bootstrap/dist/css'));
   };
