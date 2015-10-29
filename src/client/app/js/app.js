7ee3871fc95551a2a7ecb635642e3252d0b1fb90
Gabriel Malet <gabrielmalet@gmail.com>
2015-10-29 16:52:52 +0100

    Add translations / Partials etc.

--- src/client/app/js/app.js
+++ src/client/app/js/app.js
@@ -18,5 +18,12 @@ var app = angular.module('feedme', ['ui.router', 'ui.bootstrap', 'feedme.control
 app.config(require('./config'))
   .run(function($rootScope, $state) {
     $rootScope.$state = $state;
+    $rootScope.rightSidebar = false;
+    // Fixture
+    $rootScope.user = {
+      firstName: 'Tyler',
+      lastName: 'Durden',
+      job: 'Founder of Chaos Project',
+    }
   })
 ;
