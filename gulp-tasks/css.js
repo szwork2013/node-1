f6dcefb21a6ec2725386c7810cb97a0293dc3a42
Gabriel Malet <gabrielmalet@gmail.com>
2015-10-30 14:56:39 +0100

    Disable url rebasing

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -3,7 +3,7 @@
 module.exports = function(gulp, plugins, npmPackages, config) {
   return function() {
     var stream = gulp.src([config.path.public + '/lib/**/*.css', 'src/client/app/css/**/*.css'])
-      .pipe(plugins.concatCss(config.env.debug ? 'app.css' : 'app.min.css'));
+      .pipe(plugins.concatCss(config.env.debug ? 'app.css' : 'app.min.css', { rebaseUrls: false }));
 
     if (!config.env.debug) {
       stream
