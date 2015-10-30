b9b935b955c145d914ad030101f47f0820acd9ca
Gabriel Malet <gabrielmalet@gmail.com>
2015-10-30 10:43:13 +0100

    Add socket.io route

--- src/server/controller/index.js
+++ src/server/controller/index.js
@@ -1,6 +1,6 @@
-module.exports.controller = function(app, config) {
-  'use strict';
+'use strict';
 
+module.exports.controller = function(app, config) {
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, config.path.public + '/index.html'));
   });
---
+++ src/server/socket/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var users = {};
+
+module.exports = function(io, conf) {
+  io.on('connection', function(socket) {
+
+    var uuid = guid();
+    users[uuid] = socket.uuid = uuid;
+
+    console.log('Client ' + uuid + ' connected');
+    socket.emit('server.user.notify', socketMsg('Welcome ' + socket.uuid, 'info'));
+    socket.broadcast.emit('server.user.notify', socketMsg('User ' + socket.uuid + ' connected !', 'info'));
+
+    socket.on('disconnect', function () {
+      console.log('Client ' + uuid + ' deconnected');
+      delete users[socket.username];
+    });
+
+    /*
+     * Helpers
+     */
+
+    function socketMsg(content, type) {
+      return { date: (new Date()).toString(), type: type, user: socket.uuid, message: content };
+    }
+
+    function guid() {
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+    }
+  });
+};
