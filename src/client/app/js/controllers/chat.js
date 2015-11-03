045bd3c4efc06e1eaef7cb9ac9e97fe91e6cb407
Gabriel Malet <gabrielmalet@gmail.com>
2015-11-03 11:30:19 +0100

    Add badge feature to finalize chat

--- src/client/app/js/controllers/chat.js
+++ src/client/app/js/controllers/chat.js
@@ -2,17 +2,30 @@
 
 module.exports = function($rootScope, $scope, socket) {
   $scope.messages = [];
+  $scope.isChatOpen = false;
+  $scope.msgBadge = 0;
+
   socket.on('server.users.number', function(data) {
     $scope.userNumber = data.message;
   });
+
   socket.on('client.chat.push.message', function(data) {
     $scope.messages.push(data);
     $rootScope.$broadcast('chat.scroll.bottom', {});
+    if (!$scope.isChatOpen) {
+      $scope.msgBadge++;
+    }
   });
+
   $scope.sendMessage = function() {
     if ($scope.message != '') {
       socket.emit('server.chat.push.message', $scope.message);
       $scope.message = '';
     }
-  }
+  };
+
+  $scope.openChat = function() {
+    $scope.isChatOpen = !$scope.isChatOpen;
+    $scope.msgBadge = 0;
+  };
 };
