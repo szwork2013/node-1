b6326182a7677163579655ef85587025cee461e2
Gabriel Malet <gabrielmalet@gmail.com>
2015-11-04 09:56:22 +0100

    Add logout feature with browser storage destroy

--- src/client/app/js/controllers/login.js
+++ src/client/app/js/controllers/login.js
@@ -2,15 +2,22 @@
 
 var toastr = require('toastr');
 
-module.exports = function ($scope, $http, auth, store, $location) {
-  $scope.login = function () {
-    auth.signin({}, function (profile, token) {
-      // Success callback
-      store.set('profile', profile);
-      store.set('token', token);
-      $location.path('/');
-    }, function(error) {
-      console.log("There was an error logging in", error);
-    });
+module.exports = ['$scope', '$http', 'auth', 'store', '$location',
+  function ($scope, $http, auth, store, $location) {
+    $scope.login = function () {
+      auth.signin({}, function (profile, token) {
+        store.set('profile', profile);
+        store.set('token', token);
+        $location.path('/');
+      }, function(error) {
+        console.log("There was an error logging in", error);
+      });
+    };
+
+    $scope.logout = function() {
+      auth.signout();
+      store.remove('profile');
+      store.remove('token');
+    }
   }
-};
+];
