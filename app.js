9b5417150840af0c31a018613cef089860a874b7
Gabriel Malet <gabrielmalet@gmail.com>
2015-11-05 11:55:05 +0100

    Add mongoose for mongo connection

--- app.js
+++ app.js
@@ -20,11 +20,24 @@ var port = 3000, host = config.env.host || 'localhost';
 app.use(bodyParser.json());
 app.use(express.static(config.path.public));
 
+// db
+mongoose.connect(config.mongo.uri);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function (callback) { console.log('Connection OK.'); });
+
+// Add models
+fs.readdirSync(config.path.server.model).forEach(function (file) {
+  if(file.substr(-3) == '.js') {
+    require(config.path.server.model + '/' + file);
+  }
+});
+
 // Add routes from controller file
 fs.readdirSync(config.path.server.controller).forEach(function (file) {
   if(file.substr(-3) == '.js') {
-    var route = require(config.path.server.controller + '/' + file);
-    route.controller(app, config);
+    require(config.path.server.controller + '/' + file).controller(app, config);
   }
 });
 
