d1303749538a9bb0b2d51ae265f3ab16712a6f6a
Gabriel Malet <gabrielmalet@gmail.com>
2015-11-05 11:55:44 +0100

    Delete files

--- src/server/socket/chat.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(io, socket, socketMsg) {
-
-  socket.on('server.chat.push.message', function(data) {
-    io.sockets.emit('client.chat.push.message', socketMsg(data));
-  });
-};
